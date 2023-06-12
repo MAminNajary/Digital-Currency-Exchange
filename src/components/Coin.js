f9c8dae43f5c167538f2f33b82e9558586e507a1
M.Amin Najary <mohammadaminnajary@gmail.com>
2023-06-12 12:36:25 +0330

    Add styling to coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
+// styles
+import styles from '../styles/Coin.module.css';
+
 const Coin = ({img, name, symbol, price, marketCap, priceChange}) => {
     return (
-        <div>
+        <div className={styles.container}>
             <img src={img} alt={name} />
             <span>{name}</span>
             <span>{symbol.toUpperCase()}</span>
-            <span>{price.toLocaleString()}</span>
-            <span>{priceChange}</span>
-            <span>{marketCap.toLocaleString()}</span>
+            <span>$ {price.toLocaleString()}</span>
+            <span className={priceChange > 0 ? styles.greenPrice : styles.redPrice}>% {priceChange}</span>
+            <span>$ {marketCap.toLocaleString()}</span>
         </div>
     );
 };
